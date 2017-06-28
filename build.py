05465c5e31399561e30026d3bb48cf23f9504580
Joshua Webb <joshua.webb@serraview.com>
2017-06-28 17:23:28 +1000

    Refactor YAML writing
    - I know writing the YAML block with a heredoc is pretty ghetto, but it
      produces nicer looking results and is pretty readable. It's pretty
      fragile with regards to composition / levels of indentation, and
      if those are likely to change this will need to be revisited.
    
    - Allow shebangs to exit the previous language

--- build.py
+++ build.py
@@ -1,69 +1,71 @@
 #!/usr/bin/env python
 
 import yaml
-import collections
 import re
 
-# https://stackoverflow.com/a/21912744/1831350
-from collections import OrderedDict
-def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwds):
-    class OrderedDumper(Dumper):
-        pass
-    def _dict_representer(dumper, data):
-        return dumper.represent_mapping(
-            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-            data.items())
-    OrderedDumper.add_representer(OrderedDict, _dict_representer)
-    return yaml.dump(data, stream, OrderedDumper, **kwds)
+def write_rule(stream, match, push, otherLanguageStart, shebangStart):
+    stream.write("""\
+    - match: '{match}'
+      scope: comment.line
+      captures:
+        '1': meta.separator
+      push: '{push}'
+      with_prototype:
+        - match: '{otherLanguageStart}'
+          pop: true
+        - match: '{shebangStart}'
+          pop: true
 
-main = []
-with open("languages.yaml", 'r') as inStream:
-    data = yaml.load(inStream)
-    allMarkers = set()
+""".format(
+        match = match,
+        push = push,
+        otherLanguageStart = otherLanguageStart,
+        shebangStart = shebangStart
+    ))
 
-    for item in data["languages"]:
-        allMarkers.update(set(item["markers"]))
+with open("MultiSyntax.sublime-syntax", 'w') as outStream:
+    outStream.write("""\
+%YAML 1.2
+---
+name: Multi Language
+file_extensions: [untitled]
+scope: meta.multilang
+uuid: BB3B672F-2ABB-496E-9D19-E9F1C3F082D1
 
-    otherLanguages = '|'.join([re.escape(x) for x in allMarkers])
+variables:
+  comment: '^\s*//\s*(?i)'
+  # variables are not supported inside `with_prototype`
+  # https://github.com/SublimeTextIssues/Core/issues/1488
 
-    # TODO add 'plain(text)?' ??
-    otherLanguageStart = '^(?=\s*//\s*(?i)('+otherLanguages+')$)'
+contexts:
+  main:
+""")
 
-    for item in data["languages"]:
-        uniqueMarkers = set(item["markers"])
-        markers = '|'.join([re.escape(x) for x in uniqueMarkers])
-        match = '{{comment}}(?i)('+ markers +')$'
+    with open("languages.yaml", 'r') as inStream:
+        main = []
+        data = yaml.load(inStream)
+        allMarkers = set()
+        shebangMarkers = set()
+        for item in data["languages"]:
+            allMarkers.update(set(item["markers"]))
+            if (item.get("shebang")):
+                shebangMarkers.update(set(item["markers"]))
 
-        rule = collections.OrderedDict()
-        rule['match'] = match
-        rule['scope'] = 'comment.line'
-        rule['captures'] = {1 : 'meta.separator'}
-        rule['push'] = item["push"]
+        otherLanguages = '|'.join([re.escape(x) for x in allMarkers])
+        shebangLanguages = '|'.join([re.escape(x) for x in shebangMarkers])
 
-        prototype = [collections.OrderedDict()]
-        prototype[0]['match'] = otherLanguageStart
-        prototype[0]['pop'] = True
+        otherLanguageStart = r'^(?=//\s*(?i)(' + otherLanguages + r')$)'
+        shebangStart = r'^(?=#!/.*\b(?i)(' + shebangLanguages + r')\b)'
 
-        rule['with_prototype'] = prototype
-        main.append(rule)
+        for item in data["languages"]:
+            uniqueMarkers = set(item["markers"])
+            markers = '|'.join([re.escape(x) for x in uniqueMarkers])
+            match = r'{{comment}}(?i)('+ markers + r')$'
+            push = item["push"]
 
-        if not item.get("shebang"):
-            continue
+            write_rule(outStream, match, push, otherLanguageStart, shebangStart)
 
-        import copy
-        rule = copy.deepcopy(rule)
-        rule['match'] = '^#!/.*(?i)(' + markers + ')'
-        main.append(rule)
-
-    contexts = { "main" : main }
-    data = collections.OrderedDict()
-
-    data["name"] = "Multi Language"
-    data["file_extensions"] = ["untitled"]
-    data["scope"] = 'source.multi'
-    data["uuid"] = 'BB3B672F-2ABB-496E-9D19-E9F1C3F082D1'
-    data["variables"] = { "comment": '^\s*//\s*(?i)' }
-    data["contexts"] = contexts
-
-    with open("MultiSyntax.sublime-syntax", 'w') as outStream:
-        print ordered_dump(data, outStream, default_flow_style=False, explicit_start=True, version=(1,2))
+            if item.get("shebang"):
+                shebang_match = r'^#!/.*\b('+ markers + r')\b'
+                outStream.write("    # shebang version\n")
+                write_rule(outStream, shebang_match, push, otherLanguageStart, shebangStart)
