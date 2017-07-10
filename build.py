1ac078ac4f6a9d38d13f240cee0a37678ce67e2f
Joshua Webb <joshua.webb@serraview.com>
2017-07-10 17:06:20 +1000

    Refactor
    - Variables don't work directly in `with_prototype`, but they do work
      from an included context. This lets us reduce duplication in the
      output too!
    
    - Restructure the layout of the python methods to resemble the shape of
      the document being generated.

--- build.py
+++ build.py
@@ -3,23 +3,8 @@
 import yaml
 import re
 
-def write_rule(stream, match, push, otherLanguageStart, shebangStart):
+def write_syntax_start(stream):
     stream.write("""\
-    - match: '{match}'
-      scope: comment.line
-      captures:
-        '1': meta.separator
-      push: '{push}'
-      with_prototype:
-        - match: '{otherLanguageStart}'
-          pop: true
-        - match: '{shebangStart}'
-          pop: true
-
-""".format(**locals()))
-
-with open("MultiSyntax.sublime-syntax", 'w') as outStream:
-    outStream.write("""\
 %YAML 1.2
 ---
 name: Multi Language
@@ -28,13 +13,35 @@ scope: meta.multilang
 uuid: BB3B672F-2ABB-496E-9D19-E9F1C3F082D1
 
 variables:
-  comment: '^\s*//\s*(?i)'
-  # variables are not supported inside `with_prototype`
-  # https://github.com/SublimeTextIssues/Core/issues/1488
+  comment: '^\s*//\s*?(?i)'
 
 contexts:
-  main:
 """)
+
+def write_prototype_context(stream, contextName, otherLanguageStart, shebangStart):
+    stream.write("""\
+  {contextName}:
+    - match: '{otherLanguageStart}'
+      pop: true
+    - match: '{shebangStart}'
+      pop: true
+
+""".format(**locals()))
+
+def write_rule(stream, otherLanguageStartRuleName, match, push):
+    stream.write("""\
+    - match: '{match}'
+      scope: comment.line
+      captures:
+        '1': meta.separator
+      push: '{push}'
+      with_prototype:
+        - include: {otherLanguageStartRuleName}
+
+""".format(**locals()))
+
+with open("MultiSyntax.sublime-syntax", 'w') as outStream:
+    write_syntax_start(outStream)
 
     with open("languages.yaml", 'r') as inStream:
         main = []
@@ -46,21 +53,24 @@ contexts:
             if (item.get("shebang")):
                 shebangMarkers.update(set(item["markers"]))
 
+        otherLanguageStartRuleName = 'otherLanguageStart'
         otherLanguages = '|'.join([re.escape(x) for x in allMarkers])
         shebangLanguages = '|'.join([re.escape(x) for x in shebangMarkers])
 
-        otherLanguageStart = r'^(?=//\s*(?i)(' + otherLanguages + r')$)'
+        otherLanguageStart = r'^(?={{comment}}(?i)(' + otherLanguages + r')$)'
         shebangStart = r'^(?=#!/.*\b(?i)(' + shebangLanguages + r')\b)'
 
+        write_prototype_context(outStream, otherLanguageStartRuleName, otherLanguageStart, shebangStart)
+        outStream.write("  main:\n")
         for item in data["languages"]:
             uniqueMarkers = set(item["markers"])
             markers = '|'.join([re.escape(x) for x in uniqueMarkers])
             match = r'{{comment}}(?i)('+ markers + r')$'
             push = item["push"]
 
-            write_rule(outStream, match, push, otherLanguageStart, shebangStart)
+            write_rule(outStream, otherLanguageStartRuleName, match, push)
 
             if item.get("shebang"):
                 shebang_match = r'^#!/.*\b('+ markers + r')\b'
                 outStream.write("    # shebang version\n")
-                write_rule(outStream, shebang_match, push, otherLanguageStart, shebangStart)
+                write_rule(outStream, otherLanguageStartRuleName, shebang_match, push)
